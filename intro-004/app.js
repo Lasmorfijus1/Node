fa00e003264c612b2c91a887c499b7f8adbeb02a
Lasmorfijus1 <lasmorfijus@gmail.com>
2022-01-12 21:17:33 +0200

    Promises explained

--- intro-004/app.js
+++ intro-004/app.js
@@ -1,3 +1,14 @@
 console.log('Hello world');
 console.log('Naujas išvedimas');
+console.log('Naujas išvedimas');
+console.log('Naujas išvedimas');
+console.log('d');
+console.log('d');
+console.log('d');
+console.log('d');
+console.log('d');
+console.log('d');
 console.log('dsds');
+
+// for of - masyvams
+// for in - objektams 
